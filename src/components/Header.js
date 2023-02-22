09cdcbd8646170b3dbdef744522caab2027777ac
Roman-theorist <romanoleynikov@gmail.com>
2023-02-22 20:23:43 +0100

    Split project in components

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <div className="Header">
+            <div className="Logo"></div>
+            <div className="Navigation"></div>
+            <a href="/">Home</a>
+            <a href="/favorites">Favorites</a>
+            <a href="/Upcoming">Upcoming</a>
+            <div className="Search"></div>
+            
+            
+        </div>
+    )
+}
+
+export default Header 
