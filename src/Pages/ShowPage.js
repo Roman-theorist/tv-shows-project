dfa745af05575b4f37f3aa06c8549c8a7e0580b9
Roman-theorist <romanoleynikov@gmail.com>
2023-03-29 20:26:29 +0200

    styles, selectors

--- src/Pages/ShowPage.js
+++ src/Pages/ShowPage.js
@@ -1,15 +1,22 @@
 import {useParams} from "react-router-dom"
 import {getShowByID} from "../helpers/showsHelper" 
+import NotFound from "./NotFound"
 
 function ShowPage () {
     const params = useParams()
     const show = getShowByID(params.showID)
-        return (
+    
+    if (!show) {
+        return (<NotFound/>)
+    }
+    
+    return (
         <div className="ShowPage">
             <h1> { show.name } </h1>
             <img src={show.image.medium} ></img>
             <h2> Language: {show.language}</h2>
             <h3>Raiting: {show.rating.average}</h3>
+            <h3>Ganres: {show.genres.join(', ')}</h3>
         </div>
     )
 }
