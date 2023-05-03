fe7138306250c90413194d77b64c6706ac727529
Roman-theorist <romanoleynikov@gmail.com>
2023-05-03 20:12:38 +0200

    modifeid Show

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -3,11 +3,22 @@ import { searchForShow } from "../helpers/showsHelper"
 import ShowList from "../components/ShowList"
 import {useState} from "react";
 import {useParams} from "react-router-dom"
-
+import { useEffect, useState } from "react";
 
 function Search(){
     const params=useParams()
-    const searchResults=searchForShow(params.searchText)
+
+    const [searchResults, setSearchResults] = useState([])
+
+    useEffect( () => {
+        const fetchResults = async () => {
+            const shows = await searchResults(params.searchText)
+            setSearchResults(shows)
+        }
+        fetchResults()
+    }, [params.searchText])
+
+//    const searchResults=searchForShow(params.searchText)
 
 //    const allShow =searchForShow('')
 //    const [searchResults,setSearchResults] = useState(allShow)
--- src/helpers/showsHelper.js
+++ src/helpers/showsHelper.js
@@ -1,6 +1,14 @@
 import showsData from "../shows-data"
 import showData from "../shows-data"
 
+const defaultImg = { medium: "http://via.placeholder.com/219x295?text=:( "}
+
+function transformShow(show) {
+    const transformedShow ={...show, image: show.image || defaultImg}
+    return transformedShow
+}
+
+
 export function getShowByID (id) {
     const show=showsData.find(show => show.id==id)
     return show
@@ -10,6 +18,11 @@ export function getHomeShows() {
     return showsData
 }
 
-export function searchForShow(text){
-    return showsData.filter(show => show.name.toLowerCase().includes(text.toLowerCase()))
+export async function searchForShow(text){
+    const response = await fetch("http://api.tvmaze.com/search/show?q=" + text)
+    const jsonData = await response.json()
+    const showResults = jsonData.map( element => transformShow(element.show))
+    return showResults
+
+//    return showsData.filter(show => show.name.toLowerCase().includes(text.toLowerCase()))
 }
